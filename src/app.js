85cb052963d9f897fd929810ec7a595d13d013a8
andersonlouzada <anderson.s.louzada@gmail.com>
2023-08-25 17:40:39 -0300

    começa o requisito 03

--- src/app.js
+++ src/app.js
@@ -11,7 +11,13 @@ app.get('/', (_request, response) => {
 
 app.use(express.json());
 
-// ...
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) { 
+    return res.status(400).json({ message: 'Some required fields are missing' });
+  }
+  return res.status(200).json({ message: 'Login efetuado com sucesso' });
+});
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
