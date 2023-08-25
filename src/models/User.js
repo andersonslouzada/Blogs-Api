b76d334d7f2f52190d8c3e3211df4679007d2b22
andersonlouzada <anderson.s.louzada@gmail.com>
2023-08-25 17:32:42 -0300

    cria model User

---
+++ src/models/User.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  const UserModel = sequelize.define('User', {
+    id: { 
+      type: DataTypes.INTEGER, 
+      primaryKey: true,
+      autoIncrement: true
+    },
+    display_name: { 
+      type: DataTypes.STRING, 
+      allowNull: false
+    }, 
+    email: { 
+      type: DataTypes.STRING, 
+      allowNull: false,
+      unique: true
+    },
+    password: { 
+      type: DataTypes.STRING, 
+      allowNull: false
+    },
+    image: { 
+      type: DataTypes.STRING, 
+      allowNull: true
+    },
+  });
+
+  return UserModel;
+};
