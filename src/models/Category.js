de2179918431eab2f4b833150ebbabe4abc0f5e3
andersonlouzada <anderson.s.louzada@gmail.com>
2023-09-08 11:17:06 -0300

    cria model para a tabela categories

---
+++ src/models/Category.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  const CategoryModel = sequelize.define('Category', {
+    id: { 
+      type: DataTypes.INTEGER, 
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: { 
+      type: DataTypes.STRING, 
+      allowNull: false
+    }, 
+  },
+  {
+    tableName: 'categories',
+    timestamps: false,
+    underscored: true,
+  });
+
+  return CategoryModel;
+};
